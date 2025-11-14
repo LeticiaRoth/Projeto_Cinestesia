42f43fe0fc2520037159ee9869c758f0497712f9
LeticiaRoth <leticiaroth1@gmail.com>
2025-11-14 13:55:19 -0300

    Ajustes

--- Front/src/context/AuthContext.jsx
+++ Front/src/context/AuthContext.jsx
@@ -6,22 +6,18 @@ const AuthContext = createContext();
 // 'guest' (convidado) é mais seguro do que 'user'
 const estadoInicialUsuario = {
   nome: null,
-  role: "guest", // 'guest', 'user', ou 'admin'
+  role: "user", 
   token: null
 };
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(estadoInicialUsuario);
 
-  // Adiciona uma função de logout para limpar o usuário
   const logout = () => {
     setUser(estadoInicialUsuario);
-    // Aqui você também pode invalidar o token no backend se desejar
-    // fetch('http://localhost:8000/logout', { ... });
   };
 
   return (
-    // Agora o Contexto fornece o usuário, a função de login (setUser) e a de logout
     <AuthContext.Provider value={{ user, setUser, logout }}>
       {children}
     </AuthContext.Provider>
